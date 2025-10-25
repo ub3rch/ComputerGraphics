528d3242cd4e01b56e453b96da3dfd54539a1d49
Uberch <vam.molch@gmail.com>
2025-10-25 12:27:30 +0300

    Finish Lab 2 (with bonus)

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -144,7 +144,7 @@ namespace cg::renderer
 	{
 		width = in_width;
 		height = in_height;
-		// TODO Lab: 2.06 Add `history` resource in `raytracer` class
+		history = std::make_shared<cg::resource<float3>>(width, height);
 	}
 
 	template<typename VB, typename RT>
@@ -153,8 +153,8 @@ namespace cg::renderer
 	{
 		for (size_t i = 0; i<render_target->count(); ++i) {
 			render_target->item(i) = in_clear_value;
+			history->item(i) = float3(0.f);
 		}
-		// TODO Lab: 2.06 Add `history` resource in `raytracer` class
 	}
 
 	template<typename VB, typename RT>
@@ -176,16 +176,17 @@ namespace cg::renderer
 			auto &index_buffer = index_buffers[s];
 			auto &vertex_buffer = vertex_buffers[s];
 			size_t i=0;
+			aabb<VB> aabb;
 			while(i<index_buffer->count()) {
 				triangle <VB> triangle(
 					vertex_buffer->item(index_buffer->item(i++)),
 					vertex_buffer->item(index_buffer->item(i++)),
 					vertex_buffer->item(index_buffer->item(i++))
 				);
-				triangles.push_back(triangle);
+				aabb.add_triangle(triangle);
 			}
+			acceleration_structures.push_back(aabb);
 		}
-		// TODO Lab: 2.05 Implement `build_acceleration_structure` method of `raytracer` class
 	}
 
 	template<typename VB, typename RT>
@@ -193,22 +194,30 @@ namespace cg::renderer
 			float3 position, float3 direction,
 			float3 right, float3 up, size_t depth, size_t accumulation_num)
 	{
-		#pragma omp parallel for
-		for (int x=0; x<width; ++x) {
-			for (int y=0; y<height; ++y) {
-				float u = (2.f*x) / static_cast<float>(width-1) - 1.f;
-				float v = (2.f*y) / static_cast<float>(height-1) - 1.f;
-				u *= static_cast<float>(width)/static_cast<float>(height);
-				float3 ray_direction = direction + u*right - v*up;
+		float frame_weight = 1.f/accumulation_num;
+		for (int frame_id = 0; frame_id < accumulation_num; ++frame_id) {
+			std::cout << "Tracing frame #" << frame_id + 1 << "/" << accumulation_num << std::endl;
+			float2 jitter = get_jitter(frame_id);
+			#pragma omp parallel for
+			for (int x=0; x<width; ++x) {
+				for (int y=0; y<height; ++y) {
+					float u = (2.f*x + jitter.x) / static_cast<float>(width-1) - 1.f;
+					float v = (2.f*y + jitter.y) / static_cast<float>(height-1) - 1.f;
+					u *= static_cast<float>(width)/static_cast<float>(height);
+					float3 ray_direction = direction + u*right - v*up;
 
-				ray ray(position, ray_direction);
+					ray ray(position, ray_direction);
 
-				payload payload = trace_ray(ray, depth);
+					payload payload = trace_ray(ray, depth);
 
-				render_target->item(x, y) = RT::from_color(payload.color);
+					auto& history_pixel = history->item(x, y);
+					history_pixel += sqrt(payload.color.to_float3() * frame_weight);
+
+					if (frame_id + 1 == accumulation_num)
+						render_target->item(x, y) = RT::from_float3(history_pixel);
+				}
 			}
 		}
-		// TODO Lab: 2.06 Implement TAA in `ray_generation` method of `raytracer` class
 	}
 
 	template<typename VB, typename RT>
@@ -224,11 +233,16 @@ namespace cg::renderer
 		closest_hit_payload.t = max_t;
 		const triangle<VB>* closest_triangle = nullptr;
 
-		for(auto &triangle : triangles) {
-			payload payload = intersection_shader(triangle, ray);
-			if (payload.t > min_t && payload.t < closest_hit_payload.t) {
-				closest_hit_payload = payload;
-				closest_triangle = &triangle;
+		for (auto& aabb : acceleration_structures) {
+			if (!aabb.aabb_test(ray)) continue;
+			for(auto &triangle : aabb.get_triangles()) {
+				payload payload = intersection_shader(triangle, ray);
+				if (payload.t > min_t && payload.t < closest_hit_payload.t) {
+					if (any_hit_shader)
+						return any_hit_shader(ray, payload, triangle);
+					closest_hit_payload = payload;
+					closest_triangle = &triangle;
+				}
 			}
 		}
 
@@ -237,8 +251,6 @@ namespace cg::renderer
 				return closest_hit_shader(ray, closest_hit_payload, *closest_triangle, depth);
 			}
 		}
-		// TODO Lab: 2.04 Adjust `trace_ray` method of `raytracer` to use `any_hit_shader`
-		// TODO Lab: 2.05 Adjust `trace_ray` method of `raytracer` class to traverse the acceleration structure
 		return miss_shader(ray);
 	}
 
@@ -271,26 +283,66 @@ namespace cg::renderer
 	template<typename VB, typename RT>
 	float2 raytracer<VB, RT>::get_jitter(int frame_id)
 	{
-		// TODO Lab: 2.06 Implement `get_jitter` method of `raytracer` class
+		float2 result(0.f);
+
+		constexpr int base_x = 2;
+		int index = frame_id + 1;
+		float inv_base = 1.f/base_x;
+		float fraction = inv_base;
+		while (index > 0) {
+			result.x += (index % base_x) * fraction;
+			index /= base_x;
+			fraction *= inv_base;
+		}
+
+		constexpr int base_y = 3;
+		index = frame_id + 1;
+		inv_base = 1.f/base_y;
+		fraction = inv_base;
+		while (index > 0) {
+			result.y += (index % base_y) * fraction;
+			index /= base_y;
+			fraction *= inv_base;
+		}
+
+		return result - 0.5f;
 	}
 
 
 	template<typename VB>
 	inline void aabb<VB>::add_triangle(const triangle<VB> triangle)
 	{
-		// TODO Lab: 2.05 Implement `aabb` class
+		if (triangles.empty())
+			aabb_max = aabb_min = triangle.a;
+
+		triangles.push_back(triangle);
+
+		aabb_max = max(aabb_max, triangle.a);
+		aabb_max = max(aabb_max, triangle.b);
+		aabb_max = max(aabb_max, triangle.c);
+
+		aabb_min = min(aabb_min, triangle.a);
+		aabb_min = min(aabb_min, triangle.b);
+		aabb_min = min(aabb_min, triangle.c);
 	}
 
 	template<typename VB>
 	inline const std::vector<triangle<VB>>& aabb<VB>::get_triangles() const
 	{
-		// TODO Lab: 2.05 Implement `aabb` class
+		return triangles;
 	}
 
 	template<typename VB>
 	inline bool aabb<VB>::aabb_test(const ray& ray) const
 	{
-		// TODO Lab: 2.05 Implement `aabb` class
+		float3 inv_ray_direction = float3(1.f) / ray.direction;
+		float3 t0 = (aabb_max - ray.position) * inv_ray_direction;
+		float3 t1 = (aabb_min - ray.position) * inv_ray_direction;
+
+		float3 tmax = max(t0,t1);
+		float3 tmin = min(t0,t1);
+
+		return maxelem(tmin) <= minelem(tmax);
 	}
 
 }// namespace cg::renderer
--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -22,8 +22,15 @@ void cg::renderer::ray_tracing_renderer::init()
 	raytracer->set_vertex_buffers(model->get_vertex_buffers());
 	raytracer->set_index_buffers(model->get_index_buffers());
 
-	// TODO Lab: 2.03 Add light information to `lights` array of `ray_tracing_renderer`
-	// TODO Lab: 2.04 Initialize `shadow_raytracer` in `ray_tracing_renderer`
+	lights.push_back({
+		float3{0.f, 1.58f, -0.03f},
+		float3{0.78f, 0.78f, 0.78f}
+	});
+
+	shadow_raytracer = std::make_shared<cg::renderer::raytracer<cg::vertex, cg::unsigned_color>>();
+
+	shadow_raytracer->set_vertex_buffers(model->get_vertex_buffers());
+	shadow_raytracer->set_index_buffers(model->get_index_buffers());
 }
 
 void cg::renderer::ray_tracing_renderer::destroy() {}
@@ -36,16 +43,50 @@ void cg::renderer::ray_tracing_renderer::render()
 
 	raytracer->miss_shader = [](const ray& ray) {
 		payload payload{};
-		payload.color = {0.f, 0.f, (ray.direction.y + 1.f) * 0.5f};
+		payload.color = {0.f, 0.f, 0.f};
+		// payload.color = {0.f, 0.f, (ray.direction.y + 1.f) * 0.5f};
 		return payload;
 	};
 
-	raytracer->closest_hit_shader = [](const ray& ray, payload& payload, const triangle<cg::vertex>& triangle, size_t depth) {
-		payload.color = cg::color::from_float3(triangle.diffuse);
+	std::random_device random_device;
+	std::mt19937 random_generator(random_device());
+	std::uniform_real_distribution<float> uniform(-1.f, 1.f);
+
+	raytracer->closest_hit_shader = [&](const ray& ray, payload& payload, const triangle<cg::vertex>& triangle, size_t depth) {
+		float3 position = ray.position + payload.t * ray.direction;
+		float3  normal = normalize(
+			payload.bary.x * triangle.na +
+			payload.bary.y * triangle.nb +
+			payload.bary.z * triangle.nc
+		);
+
+		float3 result_color = triangle.emissive;
+		float3 random_direction(uniform(random_generator), uniform(random_generator), uniform(random_generator));
+		if (dot(normal, random_direction) < 0.f)
+			random_direction = - random_direction;
+
+		cg::renderer::ray to_next_object(position, random_direction);
+		auto next_payload = raytracer->trace_ray(to_next_object, depth);
+		result_color += triangle.diffuse * next_payload.color.to_float3() *
+			std::max(dot(normal, to_next_object.direction), 0.f);
+
+		payload.color = cg::color::from_float3(result_color);
 		return payload;
 	};
 
 	raytracer->build_acceleration_structure();
+
+	shadow_raytracer->miss_shader = [](const ray& ray) {
+		payload payload{};
+		payload.t = -1.f;
+		return payload;
+	};
+
+	shadow_raytracer->any_hit_shader = [](const ray& ray, payload& payload, const triangle<cg::vertex> &triangle) {
+		return payload;
+	};
+
+	shadow_raytracer->acceleration_structures = raytracer->acceleration_structures;
 
 	{
 		cg::utils::timer("Ray generation");
@@ -61,10 +102,4 @@ void cg::renderer::ray_tracing_renderer::render()
 	}
 
 	cg::utils::save_resource(*render_target, settings->result_path);
-
-	// TODO Lab: 2.03 Adjust `closest_hit_shader` of `raytracer` to implement Lambertian shading model
-	// TODO Lab: 2.04 Define `any_hit_shader` and `miss_shader` for `shadow_raytracer`
-	// TODO Lab: 2.04 Adjust `closest_hit_shader` of `raytracer` to cast shadows rays and to ignore occluded lights
-	// TODO Lab: 2.05 Adjust `ray_tracing_renderer` class to build the acceleration structure
-	// TODO Lab: 2.06 (Bonus) Adjust `closest_hit_shader` for Monte-Carlo light tracing
 }
