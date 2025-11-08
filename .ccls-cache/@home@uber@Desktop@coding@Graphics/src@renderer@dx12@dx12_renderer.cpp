8253a552db7efee633a4ca9a8f664efafb26c941
Uberch <vam.molch@gmail.com>
2025-11-08 12:55:08 +0300

    Lab 3.08

--- .ccls-cache/@home@uber@Desktop@coding@Graphics/src@renderer@dx12@dx12_renderer.cpp
+++ .ccls-cache/@home@uber@Desktop@coding@Graphics/src@renderer@dx12@dx12_renderer.cpp
@@ -33,7 +33,13 @@ void cg::renderer::dx12_renderer::destroy()
 
 void cg::renderer::dx12_renderer::update()
 {
-	// TODO Lab: 3.08 Implement `update` method of `dx12_renderer`
+	auto now = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float> duration = now - current_time;
+	frame_duration = duration.count();
+	current_time = now;
+
+	cb.mvpMatrix = camera->get_dxm_mvp_Matrix();
+	memcpy(constant_buffer_data_begin, &cb, sizeof(cb));
 }
 
 void cg::renderer::dx12_renderer::render()
